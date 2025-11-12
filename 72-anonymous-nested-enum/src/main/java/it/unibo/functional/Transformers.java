c4d1bbc2c799595e59021f01a903b6d0cdf3a1b4
Pietro Dapporto <dapportopietro@gmail.com>
2025-11-12 21:34:55 +0100

    implemented MonthSorterNested passing the tests

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -5,7 +5,6 @@ import it.unibo.functional.api.Function;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -1,21 +1,89 @@
 package it.unibo.nestedenum;
 
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Locale;
-import java.util.Objects;
 
 /**
  * Implementation of {@link MonthSorter}.
  */
 public final class MonthSorterNested implements MonthSorter {
 
+    public static final int MONTHNUMBER = 12;
+
+    public enum Month{
+        JANUARY(31), 
+        FEBRUARY(28), 
+        MARCH(31), 
+        APRIL(30), 
+        MAY(31), 
+        JUNE(30), 
+        JULY(31), 
+        AUGUST(31), 
+        SEPTEMBER(30), 
+        OCTOBER(31), 
+        NOVEMBER(30), 
+        DECEMBER(31);
+
+        private final int monthDays;
+
+        private Month(int monthDays){
+            this.monthDays = monthDays;
+        }
+        
+        public int getMonthDays() {
+            return monthDays;
+        }
+
+        public int getIndex() {
+            return this.ordinal();
+        }
+
+        public static Month fromString(String wordToCheck) {
+            if (wordToCheck == null || wordToCheck.trim().isEmpty()) {
+                throw new IllegalArgumentException("The string passed is null");
+            }
+
+            wordToCheck = wordToCheck.trim().toLowerCase();
+            final var matches = new ArrayList<Month>();
+            for (Month m : Month.values()) {
+                String monthName = m.name().toLowerCase();
+                if (monthName.startsWith(wordToCheck)) {
+                    matches.add(m);
+                }
+            }
+            if (matches.size() == 1) {
+                return matches.get(0);
+            }
+            for (Month m : Month.values()) {
+                if (wordToCheck.equals(m.name().toLowerCase())) {
+                    return m;
+                }
+            }
+            throw new IllegalArgumentException("No month matched");
+        }
+    }
+
+    private class SortByMonthOrder implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            return Integer.compare(Month.fromString(o1).getIndex(), Month.fromString(o2).getIndex());
+        }
+    }
+
+    private class SortByDate implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            return Integer.compare(Month.fromString(o1).getMonthDays(), Month.fromString(o2).getMonthDays());
+        }
+    }
+
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new SortByDate();
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new SortByMonthOrder();
     }
 }
